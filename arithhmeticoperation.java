5e8a9098c3ee88f296a6b8423fa048de65911044
yusof <djhongabarca@gmail.com>
2024-05-14 14:15:42 +0800

    fist commit

---
+++ arithhmeticoperation.java
@@ -0,0 +1,18 @@
+public class arithhmeticoperation {
+    public static void main(String[] args) {
+        int num1 = 56;
+        int num2 = 23;
+
+        int sum = num1 + num2;
+         System.out.println("Sum: " + sum);
+
+        int sub = num1 - num2;
+         System.out.println("subtraction: " + sub);
+
+        int multiply = num1 * num2;
+         System.out.println("multiply:" + multiply);
+        
+        int divide = num1 / num2;
+         System.out.println("Division: " + divide);
+    }
+}
