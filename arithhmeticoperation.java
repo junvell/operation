6af0a7bd518af135eb3bd24101ba699ad563b41e
junvell <152274961+junvell@users.noreply.github.com>
2024-05-14 15:54:08 +0800

    Update arithhmeticoperation.java

--- arithhmeticoperation.java
+++ arithhmeticoperation.java
@@ -1,7 +1,7 @@
 public class arithhmeticoperation {
     public static void main(String[] args) {
         int num1 = 45;
-        int num2 = 100;
+        int num2 = 101;
 
         int sum = num1 + num2;
          System.out.println("Sum: " + sum);
